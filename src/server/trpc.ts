ea8c088e2fae395f9d6905c062aded96d22d2c54
Andrew Huang <andrewhuang427@gmail.com>
2024-11-07 15:59:29 -0800

    add data transformer

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,11 +1,14 @@
 import { initTRPC } from "@trpc/server";
+import SuperJSON from "superjson";
 import { Context } from "./context";
 
 /**
  * Initialization of tRPC backend
  * Should be done only once per backend!
  */
-const t = initTRPC.context<Context>().create();
+const t = initTRPC.context<Context>().create({
+  transformer: SuperJSON,
+});
 
 export const createCallerFactory = t.createCallerFactory;
 
--- src/trpc/Provider.tsx
+++ src/trpc/Provider.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
-import { trpc } from "./client";
 import { httpBatchLink } from "@trpc/client";
+import { useState } from "react";
+import SuperJSON from "superjson";
+import { trpc } from "./client";
 
 export function TRPCProvider(props: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
@@ -23,13 +24,18 @@ function getTRPCClient() {
     links: [
       httpBatchLink({
         url: `${getBaseUrl()}/api/trpc`,
+        transformer: SuperJSON,
       }),
     ],
   });
 }
 
 function getBaseUrl() {
-  if (typeof window !== "undefined") return window.location.origin;
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (typeof window !== "undefined") {
+    return window.location.origin;
+  }
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
